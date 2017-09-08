f0672bd9a84cca0f9ee13c5c62c98c091757c0f8
Jeppe Juel Petersen <jjp@nanonord.dk>
2017-09-08 12:30:28 +0200

    more changes

--- debian9_install/setup.pp
+++ debian9_install/setup.pp
@@ -1,3 +1,6 @@
+# does not change path in i3 config
+
+
 $user = "jjp"
 $git_folder = "/home/${user}/git-repos"
 
@@ -29,7 +32,7 @@ package { 'vim-pathogen':
   ensure => 'latest',
 }
 
-#change shell
+
 #create config symlinks
 
 file { "/home/${user}/.Xdefaults":
@@ -46,9 +49,23 @@ file { "/home/${user}/.vimrc":
   group => $user,
 }
 
+file { "/home/${user}/.config/i3/config":
+  ensure => 'link',
+  target => "${git_folder}/mydotfiles/i3/i3_config",
+  owner => $user,
+  group => $user,
+}
+
 file { "/home/${user}/Pictures/planet_express.jpg":
   ensure => 'link',
   target => "${git_folder}/mydotfiles/backgrounds/planet_express.jpg",
   owner => $user,
   group => $user,
+}
+
+#change shell
+user { "${user}":
+  ensure => 'present',
+  shell => '/usr/bin/fish',
+  require => Package['fish'],
 }
