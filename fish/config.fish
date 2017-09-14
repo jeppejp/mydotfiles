f4a795dadcb844bb179d65e6711535cb52d44683
Jeppe Juel Petersen <jjp@nanonord.dk>
2017-09-14 09:15:11 +0200

    starting right prompt fish

--- fish/config.fish
+++ fish/config.fish
@@ -4,4 +4,15 @@ end
 
 funcsave fish_greeting
 
+function fish_right_prompt
+     set -l is_git_repository (git rev-parse --is-inside-work-tree ^/dev/null)
+     if test -n "$is_git_repository"
+         set -l branch (git symbolic-ref --short HEAD ^/dev/null; or git show-ref --head -s --abbrev | head -n1 ^/dev/null)
+        echo $branch
+    end
+
+end
+
+funcsave fish_right_prompt
+
 set -g fish_color_cwd 9fbc00
