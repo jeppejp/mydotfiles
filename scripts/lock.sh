14422d6bf65c27f63003e1547e62e50eefabf923
Jeppe Juel Petersen <jeppejp@gmail.com>
2017-09-17 22:37:13 +0200

    lock fun

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-scrot /tmp/lock.png
-convert /tmp/lock.png -resize 25% /tmp/lock-resize.png
-convert /tmp/lock-resize.png -blur 0x3 /tmp/lock-blur.png
-i3lock -f -i /tmp/lock-blur.png
+scrot -q 1 /tmp/lock.png 
+echo "scrot"
+convert /tmp/lock.png -blur 0x6 /tmp/lock-blur.png
+echo "blur"
+i3lock -f -i /tmp/lock.png
