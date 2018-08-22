1512a1ec463e44917a7aed96dc4ac67ce738b9cf
jeppejp <jeppejp@gmail.com>
2018-08-22 23:16:23 +0200

    Adding more packages and setup

--- debian9_install/setup.pp
+++ debian9_install/setup.pp
@@ -8,10 +8,19 @@ $git_folder = "/home/${user}/git-repos"
 package { 'feh':
   ensure => 'latest',
 }
+package { 'neovim':
+  ensure => 'latest',
+}
+package { 'tmux':
+  ensure => 'latest',
+}
 package { 'fish':
   ensure => 'latest',
 }
 package { 'fortune-mod':
+  ensure => 'latest',
+}
+package { 'curl':
   ensure => 'latest',
 }
 package { 'git':
@@ -31,12 +40,38 @@ file { "/root/.bashrc":
   content => "PS1=\"\\e[31m[ ROOT ] \\w\\e[0m \""
 }
 #create config symlinks
-
+file {"/home/${user}/bin/":
+  ensure => 'directory',
+  owner => $user,
+  group => $user,
+}
 
 file { "/home/${user}/.config/fish/":
   ensure => 'directory',
   owner => $user,
   group => $user,
+}
+
+file { "/home/${user}/.config/nvim/":
+  ensure => 'directory',
+  owner => $user,
+  group => $user,
+}
+
+file { "/home/${user}/.config/nvim/init.vim":
+  ensure => 'link',
+  owner => $user,
+  group => $user,
+  target => "${git_folder}/mydotfiles/vim/neovim-init.vim",
+  require => File["/home/${user}/.config/nvim/"],
+}
+
+file { "/home/${user}/.tmux.conf":
+  ensure => 'link',
+  owner => $user,
+  group => $user,
+  target => "${git_folder}/mydotfiles/tmux.conf",
+  require => Package['tmux'],
 }
 
 file { "/home/${user}/.config/fish/config.fish":
@@ -45,6 +80,12 @@ file { "/home/${user}/.config/fish/config.fish":
   group => $user,
   target => "${git_folder}/mydotfiles/fish/config.fish",
   require => File["/home/${user}/.config/fish/"],
+}
+
+exec {'vim-plug':
+    command => '/usr/bin/curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim',
+    path => '/home/${user}',
+    require => Package['curl'],
 }
 
 
